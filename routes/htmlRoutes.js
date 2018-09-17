25302fe4efac6e58976d57edbdfc82fa173e3ad9
jgallaway2011 <jgallaway2011@gmail.com>
2018-09-17 17:53:55 -0500

    Seperates waitER into two pages to prevent ability to make appointments unless logged in

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,43 +1,39 @@
 var db = require("../models");
 
-module.exports = function (app, passport, ensureLoggedIn) {
+module.exports = function(app, passport, ensureLoggedIn) {
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     db.UserInfo.findAll({}).then(function () {
       res.render("index");
     });
   });
 
   // Start Auth0 Login
-  app.get(
-    "/login",
+  app.get("/login",
     passport.authenticate("auth0", {
       scope: "openid email profile"
     }),
-    function (req, res) {
+    function(req, res) {
       res.redirect("/");
-    }
-  );
+    });
 
   // Path to to use after successful login
-  app.get(
-    "/callback",
+  app.get("/callback",
     passport.authenticate("auth0", {
       failureRedirect: "/login"
     }),
-    function (req, res) {
+    function(req, res) {
       res.redirect(req.session.returnTo || "/user");
-    }
-  );
+    });
 
   // Path to logout user
-  app.get("/logout", function (req, res) {
+  app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
   });
 
   // Get user profile
-  app.get("/user", ensureLoggedIn, function (req, res) {
+  app.get("/user", ensureLoggedIn, function(req, res) {
     res.render("userProfile", {
       user: req.user.displayName,
       picture: req.user.picture
@@ -45,37 +41,37 @@ module.exports = function (app, passport, ensureLoggedIn) {
   });
 
   // Hospital list selection page
-  app.get("/waitER_MAKE_APPOINTMENT", ensureLoggedIn, function (req, res) {
+  app.get("/waitER_MAKE_APPOINTMENT", ensureLoggedIn, function(req, res) {
     res.render("waitER_MAKE_APPOINTMENT");
   });
 
   // Hospital list selection page
-  app.get("/waitER_INFO_ONLY", function (req, res) {
+  app.get("/waitER_INFO_ONLY", function(req, res) {
     res.render("waitER_INFO_ONLY");
   });
 
   // Hospital list selection page
-  app.get("/northwestern", function (req, res) {
+  app.get("/northwestern", function(req, res) {
     res.render("northwestern");
   });
 
   // Hospital list selection page
-  app.get("/rush", function (req, res) {
+  app.get("/rush", function(req, res) {
     res.render("rush");
   });
 
   // Hospital list selection page
-  app.get("/loyolaMedicalCenter", function (req, res) {
+  app.get("/loyolaMedicalCenter", function(req, res) {
     res.render("loyolaMedicalCenter");
   });
 
   // Hospital list selection page
-  app.get("/universityOfChicago", function (req, res) {
+  app.get("/universityOfChicago", function(req, res) {
     res.render("universityOfChicago");
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
