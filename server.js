3f62b4879258e83ffa6a17b47b35f7c13d67c692
jgallaway2011 <jgallaway2011@gmail.com>
2018-09-22 09:00:43 -0500

    Commented out code for Auth0

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
-var session = require("express-session");
-var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
-var Auth0Strategy = require("passport-auth0"),
-  passport = require("passport");
+// var session = require("express-session");
+// var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+// var Auth0Strategy = require("passport-auth0"),
+//   passport = require("passport");
 
-var strategy = new Auth0Strategy(
-  {
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: "https://warm-eyrie-57570.herokuapp.com/callback" || "http://localhost:3000/callback"
-  },
-  function(accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  }
-);
+// var strategy = new Auth0Strategy(
+//   {
+//     domain: process.env.AUTH0_DOMAIN,
+//     clientID: process.env.AUTH0_CLIENT_ID,
+//     clientSecret: process.env.AUTH0_CLIENT_SECRET,
+//     callbackURL: "https://warm-eyrie-57570.herokuapp.com/callback" || "http://localhost:3000/callback"
+//   },
+//   function(accessToken, refreshToken, extraParams, profile, done) {
+//     // accessToken is the token to call Auth0 API (not needed in the most cases)
+//     // extraParams.id_token has the JSON Web Token
+//     // profile has all the information from the user
+//     return done(null, profile);
+//   }
+// );
 
 var db = require("./models");
 
@@ -28,44 +28,44 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 //session-related stuff
-var sess = {
-  secret: process.env.AUTH0_CLIENT_ID,
-  cookie: {
-    maxAge: 14 * 24 * 60 * 60 * 1000,
-    httpOnly: true
-  },
-  resave: false,
-  saveUninitialized: true
-};
+// var sess = {
+//   secret: process.env.AUTH0_CLIENT_ID,
+//   cookie: {
+//     maxAge: 14 * 24 * 60 * 60 * 1000,
+//     httpOnly: true
+//   },
+//   resave: false,
+//   saveUninitialized: true
+// };
 
-if (process.env.NODE_ENV === "production") {
-  sess.cookie.secure = true; // serve secure cookies, requires https
-}
+// if (process.env.NODE_ENV === "production") {
+//   sess.cookie.secure = true; // serve secure cookies, requires https
+// }
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
-app.use(session(sess));
-passport.use(strategy);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session(sess));
+// passport.use(strategy);
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+// passport.serializeUser(function(user, done) {
+//   done(null, user);
+// });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-//
-app.use(function(req, res, next) {
-  res.locals.loggedIn = false;
-  if (req.session.passport && typeof req.session.passport.user != 'undefined') {
-    res.locals.loggedIn = true;
-  }
-  next();
-});
+// passport.deserializeUser(function(user, done) {
+//   done(null, user);
+// });
+// //
+// app.use(function(req, res, next) {
+//   res.locals.loggedIn = false;
+//   if (req.session.passport && typeof req.session.passport.user != 'undefined') {
+//     res.locals.loggedIn = true;
+//   }
+//   next();
+// });
 
 // Handlebars
 app.engine(
@@ -78,7 +78,8 @@ app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app, passport, ensureLoggedIn);
+require("./routes/htmlRoutes")(app);
+// require("./routes/htmlRoutes")(app, passport, ensureLoggedIn);
 
 var syncOptions = { force: false };
 
