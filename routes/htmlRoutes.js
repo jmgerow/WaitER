c6b3f2a9b907a60238bb1b3844c8b2ea58fc1625
jgallaway2011 <jgallaway2011@gmail.com>
2018-09-22 08:01:30 -0500

    Fixed auth0 code issue & added javascript for hopsital tables

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,58 +1,52 @@
 var db = require("../models");
 
-module.exports = function(app) {
+  module.exports = function(app, passport, ensureLoggedIn) {
   // Load index page
   app.get("/", function(req, res) {
     db.UserInfo.findAll({}).then(function() {
       res.render("index");
     });
   });
-  // module.exports = function(app, passport, ensureLoggedIn) {
-  // // Load index page
-  // app.get("/", function(req, res) {
-  //   db.UserInfo.findAll({}).then(function() {
-  //     res.render("index");
-  //   });
-  // });
 
-  // // Start Auth0 Login
-  // app.get(
-  //   "/login",
-  //   passport.authenticate("auth0", {
-  //     scope: "openid email profile"
-  //   }),
-  //   function(req, res) {
-  //     res.redirect("/");
-  //   }
-  // );
-  //
-  // // Path to to use after successful login
-  // app.get(
-  //   "/callback",
-  //   passport.authenticate("auth0", {
-  //     failureRedirect: "/login"
-  //   }),
-  //   function(req, res) {
-  //     res.redirect("/user");
-  //   }
-  // );
-  //
-  // // Path to logout user
-  // app.get("/logout", function(req, res) {
-  //   req.logout();
-  //   res.redirect("/");
-  // });
+  // Start Auth0 Login
+  app.get(
+    "/login",
+    passport.authenticate("auth0", {
+      scope: "openid email profile"
+    }),
+    function(req, res) {
+      res.redirect("/");
+    }
+  );
+  
+  // Path to to use after successful login
+  app.get(
+    "/callback",
+    passport.authenticate("auth0", {
+      failureRedirect: "/login"
+    }),
+    function(req, res) {
+      res.redirect("/user");
+    }
+  );
+  
+  // Path to logout user
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
 
   // Get user profile
-  app.get("/user", function(req, res) {
+  app.get("/user", ensureLoggedIn, function(req, res) {
     res.render("userProfile", {
-      user: req.user,
-      picture: req.user
+      user: req.user.displayName,
+      picture: req.user.picture,
+      loggedIn: res.locals.loggedIn
     });
   });
 
   // Hospital list selection page
-  app.get("/waitER_MAKE_APPOINTMENT", function(req, res) {
+  app.get("/waitER_MAKE_APPOINTMENT", ensureLoggedIn, function(req, res) {
     res.render("waitER_MAKE_APPOINTMENT");
   });
 
@@ -62,22 +56,22 @@ module.exports = function(app) {
   });
 
   // Hospital list selection page
-  app.get("/northwestern", function(req, res) {
+  app.get("/northwestern", ensureLoggedIn, function(req, res) {
     res.render("northwestern");
   });
 
   // Hospital list selection page
-  app.get("/rush", function(req, res) {
+  app.get("/rush", ensureLoggedIn, function(req, res) {
     res.render("rush");
   });
 
   // Hospital list selection page
-  app.get("/loyolaMedicalCenter", function(req, res) {
+  app.get("/loyolaMedicalCenter", ensureLoggedIn, function(req, res) {
     res.render("loyolaMedicalCenter");
   });
 
   // Hospital list selection page
-  app.get("/universityOfChicago", function(req, res) {
+  app.get("/universityOfChicago", ensureLoggedIn, function(req, res) {
     res.render("universityOfChicago");
   });
 
